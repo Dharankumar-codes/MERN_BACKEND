da281d988b69857a0a2342b56d97e6298fc89921
dharankumar <dharankumar>
2026-09-09 14:12:11 +0530

    updated

---
+++ Js/Day1/Script.js
@@ -0,0 +1,34 @@
+var accountId;
+var firstPart;
+var secondPart;
+var loginName;
+var currentAge;
+var contactNo;
+var mailBox;
+var residence;
+var district;
+var postalCode;
+
+let subjectName;
+let classTiming;
+let tuitionAmount;
+let mentor;
+let roomNumber;
+let presentDays;
+let testResult;
+let performanceGrade;
+
+let workerNumber;
+let jobTitle;
+let incomeAmount;
+let joiningDate;
+
+let itemNumber;
+let itemDescription;
+let unitCost;
+let unitsPurchased;
+let payableAmount;
+
+const finalScore = 90;
+
+console.log(finalScore);
